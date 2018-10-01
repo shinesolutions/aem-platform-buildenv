b3b373e79d124cbb880752e630691021c37ff568
Cliffano Subagio <cliffano@gmail.com>
2018-10-01 12:50:06 +1000

    Add pdk installation. Update Puppet5 rpm URl.

--- provisioners/puppet.sh
+++ provisioners/puppet.sh
@@ -2,8 +2,8 @@
 set -o nounset
 set -o errexit
 
-rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet-release-el-7.noarch.rpm
-yum -y install puppet-agent epel-release
+rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
+yum -y install puppet-agent pdk epel-release
 yum -y groupinstall 'Development Tools'
 
 # Enable the rhui-REGION-rhel-server-optional to install ruby-devel
@@ -17,3 +17,4 @@ yum -y install which make ruby-devel zlib-devel
 /opt/puppetlabs/puppet/bin/puppet --version
 /opt/puppetlabs/puppet/bin/facter --version
 /opt/puppetlabs/puppet/bin/hiera --version
+/opt/puppetlabs/pdk/bin/pdk --version

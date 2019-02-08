fea7136941a83feaaf49756a95f0470d70e8f784
Cliffano Subagio <cliffano@gmail.com>
2019-02-08 11:43:05 +1100

    Add most recent botocore 1.11.x required by aws-google-auth due to range < 1.12 .

--- provisioners/aem-platform-buildenv-sandpit.pp
+++ provisioners/aem-platform-buildenv-sandpit.pp
@@ -10,6 +10,10 @@ include pip
 pip::install { 'aws-google-auth[u2f]':
   ensure         => present,
   python_version => '2.7',
+} -> pip::install { 'botocore':
+  ensure         => present,
+  version        => '1.11.9', # most recent 1.11.x required by aws-google-auth due to range < 1.12
+  python_version => '2.7',
 }
 
 class { 'cred::google':

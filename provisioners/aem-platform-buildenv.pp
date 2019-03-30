78b8c08d1b64500bbcb27467f2f8fe9eae2874f7
Cliffano Subagio <cliffano@gmail.com>
2019-03-31 00:50:40 +1100

    Add nestor node tool. Add comments for node packages usage.

--- provisioners/aem-platform-buildenv.pp
+++ provisioners/aem-platform-buildenv.pp
@@ -1,10 +1,27 @@
 class { 'nodejs':
   repo_url_suffix       => '8.x',
   nodejs_package_ensure => '8.10.0',
-} -> package { ['open-sesame', 'rtk']:
-  ensure   => 'present',
+
+# nestor is used for triggering and integration testing AEM OpenCloud Manager
+# Jenkins pipelines, it is written in node.js, and we ended up using this
+# because there was no Python-based Jenkins CLI that works with Jenkins crumb.
+} -> package { 'nestor':
+  ensure   => '2.1.0',
+  provider => 'npm',
+
+# open-sesame is used as a convenient utility tool for allowing inbound rule
+# from the public IP where the engineer is working from, it's only used for
+# build and development purposes
+} -> package { 'open-sesame':
+  ensure   => '1.1.0',
+  provider => 'npm',
+
+# rtk is used for automating repo release process (versioning, tagging)
+} -> package { 'rtk':
+  ensure   => '0.1.0',
   provider => 'npm',
 }
+
 
 package { ['git', 'ShellCheck', 'unzip', 'wget', 'docker', 'jq', 'python-devel']:
   ensure   => 'present',

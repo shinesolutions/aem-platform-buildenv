81773bc687b94f03c63a4608ae743b77e77119a5
Michael Bloch <michael.bloch@shinesolutions.com>
2020-11-27 14:50:04 +1100

    Upgrade Puppet to 5.5.22
    ### Changed
    Upgrade Puppet to 5.5.22

--- provisioners/puppet.sh
+++ provisioners/puppet.sh
@@ -3,7 +3,7 @@ set -o nounset
 set -o errexit
 
 rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
-yum -y install puppet-agent-5.5.21 || echo $?
+yum -y install puppet-agent-5.5.22 || echo $?
 
 # Info
 /opt/puppetlabs/puppet/bin/ruby --version

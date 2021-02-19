b9fe904b57caf8d2cbbdaff1e87891908056032d
hoomaan-kh <ho.kheirabadi@gmail.com>
2021-02-19 11:59:11 +1100

    Update PIP, AWSCLI and Boto3 to the latest version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Unreleased
+### Added
+- Added a step to install latest version of PIP , AWSCLI and Boto3
 
 ## 1.7.1 - 2020-11-27
 ### Changed
--- provisioners/aem-platform-buildenv.pp
+++ provisioners/aem-platform-buildenv.pp
@@ -5,3 +5,15 @@ class { '::phantomjs':
   source_dir      => '/opt',
   timeout         => 600,
 }
+
+exec { 'pip3 install --upgrade pip==21.0.1':
+path => ['/bin', '/usr/local/bin', '/usr/bin'],
+}
+package{ 'boto3':
+  ensure   => '1.17.8',
+  provider => pip3,
+}
+package{ 'awscli':
+  ensure   => '1.19.8',
+  provider => pip3,
+}

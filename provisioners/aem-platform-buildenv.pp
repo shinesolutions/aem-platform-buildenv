c3304f479b2856dfcdfc844e6f22065b6f124250
Michael Bloch <michael.bloch@shinesolutions.com>
2022-08-23 15:37:04 +1000

    Fix lint errors

--- provisioners/aem-platform-buildenv.pp
+++ provisioners/aem-platform-buildenv.pp
@@ -7,77 +7,77 @@ class { '::phantomjs':
 }
 
 # Install Python dependencies
-package { "awscli":
+package { 'awscli':
   ensure   => '1.22.1',
   provider => 'pip3',
 }
-package { "boto":
+package { 'boto':
   ensure   => '2.49.0',
   provider => 'pip3',
 }
-package { "boto3":
+package { 'boto3':
   ensure   => '1.20.0',
   provider => 'pip3',
 }
-package { "botocore":
+package { 'botocore':
   ensure   => '1.23.1',
   provider => 'pip3',
 }
-package { "jinja2":
+package { 'jinja2':
   ensure   => '2.11.3',
   provider => 'pip3',
 }
-package { "pylint":
+package { 'pylint':
   ensure   => '2.6.0',
   provider => 'pip3',
 }
-package { "ruamel.yaml":
+package { 'ruamel.yaml':
   ensure   => '0.16.5',
   provider => 'pip3',
 }
-package { "yamllint":
+package { 'yamllint':
   ensure   => '1.19.0',
   provider => 'pip3',
 }
 
 # Install Ruby dependencies
-package { "bundler":
+package { 'bundler':
   ensure   => '1.17.3',
   provider => 'puppet_gem',
 }
-package { "aws-sdk-core":
+package { 'aws-sdk-core':
   ensure   => '3.50.0',
   provider => 'puppet_gem',
 }
-package { "rake":
+package { 'rake':
   ensure   => '12.3.3',
   provider => 'puppet_gem',
 }
-package { "ruby_aem":
+package { 'ruby_aem':
   ensure   => '3.13.1',
   provider => 'puppet_gem',
 }
-package { "ruby_aem_aws":
+package { 'ruby_aem_aws':
   ensure   => '2.2.1',
   provider => 'puppet_gem',
 }
-package { "inspec":
+package { 'inspec':
   ensure   => '1.51.6',
   provider => 'puppet_gem',
 }
-package { "capybara":
+package { 'capybara':
   ensure   => '3.30.0',
   provider => 'puppet_gem',
 }
-package { "file_utils":
+package { 'file_utils':
   ensure   => '1.1.2',
   provider => 'puppet_gem',
 }
-package { "rspec":
+package { 'rspec':
   ensure   => '3.8.0',
   provider => 'puppet_gem',
 }
-package { "poltergeist":
+package { 'poltergeist':
   ensure   => '1.18.1',
   provider => 'puppet_gem',
 }

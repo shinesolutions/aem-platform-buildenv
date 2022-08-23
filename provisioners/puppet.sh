85545e124870580d9737a401d04838ca030ba230
Michael Bloch <michael.bloch@shinesolutions.com>
2022-08-23 15:04:34 +1000

    Added AEM OpenCloud library dependencies
    ### Added
    - Added new shell provisioning script `python.sh` to upgrade pip3 & install ansible
    - Added AEM OpenCloud library dependencies to aem-platform-buildenv so they are included in the docker image and don't need to be resolved
    
    ### Changed
    - Upgraded Puppet to 7.9.0
    - Upgraded dependencies in requirements.txt

--- provisioners/puppet.sh
+++ provisioners/puppet.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 set -o nounset
 set -o errexit
+PUPPET_MAJOR_VERSION=7
+PUPPET_MINOR_VERSION=9
+PUPPET_PATCH_VERSION=0
+PUPPET_AGENT_VERSION="${PUPPET_MAJOR_VERSION}.${PUPPET_MINOR_VERSION}.${PUPPET_PATCH_VERSION}"
+ARCH_TYPE=x86_64
+OS_TYPE=el
+OS_VERSION=7
 
-rpm -ivh --force https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
-yum -y install puppet-agent-5.5.22 || echo $?
+if [[ "$(/opt/puppetlabs/puppet/bin/puppet --version)" == "${PUPPET_AGENT_VERSION}" ]]
+then
+    echo "Puppet version ${PUPPET_AGENT_VERSION} is already installed. Skipping Puppet installation..."
+else
+    echo "Installing puppet ${PUPPET_AGENT_VERSION}..."
+    yum -y install "https://yum.puppetlabs.com/puppet${PUPPET_MAJOR_VERSION}/${OS_TYPE}/${OS_VERSION}/${ARCH_TYPE}/puppet-agent-${PUPPET_AGENT_VERSION}-1.${OS_TYPE}${OS_VERSION}.${ARCH_TYPE}.rpm"
+fi
 
 # Info
 /opt/puppetlabs/puppet/bin/ruby --version
